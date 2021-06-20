a2dcd5d168d67eb526c4123024a6c2812264db00
Mark Tutkowski <mark@tutkowski.com>
2021-06-20 15:54:22 -0500

    Add the no sandbox flag

--- scripts/create-pdf.js
+++ scripts/create-pdf.js
@@ -3,18 +3,21 @@ const path = require('path');
 const puppeteer = require('puppeteer');
  
 const generatePdfFromUrl = async (url) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+    headless: true,
+    args: ['--no-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto(url, {waitUntil: 'networkidle0'});
   const pdf = await page.pdf({ format: 'Letter', printBackground: true });
  
   await browser.close();
-  return pdf
+  return pdf;
 }
 
-const buildDir = path.join(__dirname, '..', 'build')
-const sourceHtmlUrl = 'file://' + path.join(buildDir, 'resume.html')
-const pdf = path.join(buildDir, 'mark_tutkowski.pdf')
+const buildDir = path.join(__dirname, '..', 'build');
+const sourceHtmlUrl = 'file://' + path.join(buildDir, 'resume.html');
+const pdf = path.join(buildDir, 'mark_tutkowski.pdf');
 
-console.log(`Generating PDF from ${sourceHtmlUrl} to ${pdf}`)
-generatePdfFromUrl(sourceHtmlUrl).then(buffer => fs.writeFileSync(pdf, buffer))
+console.log(`Generating PDF from ${sourceHtmlUrl} to ${pdf}`);
+generatePdfFromUrl(sourceHtmlUrl).then(buffer => fs.writeFileSync(pdf, buffer));
