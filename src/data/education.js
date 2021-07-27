a5adafa8b42f057050049b8e91b438eef5e0085a
Mark Tutkowski <mark@tutkowski.com>
2021-07-27 15:33:10 -0500

    Add masters program

--- src/data/education.js
+++ src/data/education.js
@@ -1,5 +1,11 @@
 const eduction = [
   {
+    degree: 'Master of Computer Science',
+    institution: 'University of Illinois at Urbana-Champaign',
+    startDate: 'Aug 2021',
+    endDate: 'Present',
+  },
+  {
     degree: 'Bachelor of Science in Computer Engineering',
     institution: 'Milwaukee School of Engineering',
     startDate: 'Sept 2011',
