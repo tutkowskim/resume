4db460d080c67d48df1e9dd9d857ed1a810c0519
Mark Tutkowski <tutkows1@gmail.com>
2022-07-17 21:47:35 -0500

    Fixup header

--- src/ContactInformation.jsx
+++ src/ContactInformation.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { faPhone, faEnvelope, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { personalInformation } from './data';
 
 function ContactInformation() {
@@ -9,32 +8,20 @@ function ContactInformation() {
     <>
       <label>
         <a className="contact-info-link" href={`tel:${personalInformation.phoneNumber}`} color="inherit" variant="body2">
-          <FontAwesomeIcon icon={faPhone} />
+          <FontAwesomeIcon className="contact-info-icon" icon={faPhone} />
           <span>{personalInformation.phoneNumber}</span>
         </a>
       </label>
       <label>
         <a className="contact-info-link" target="_blank" href={`mailto:${personalInformation.email}`} rel="noreferrer" color="inherit" variant="body2">
-          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon className="contact-info-icon" icon={faEnvelope} />
           <span>{personalInformation.email}</span>
         </a>
       </label>
       <label>
         <a className="contact-info-link" target="_blank" href={personalInformation.website.url} rel="noreferrer" color="inherit" variant="body2">
-          <FontAwesomeIcon icon={faGlobe} />
+          <FontAwesomeIcon className="contact-info-icon" icon={faGlobe} />
           <span>{personalInformation.website.display}</span>
-        </a>
-      </label>
-      <label>
-        <a className="contact-info-link" target="_blank" href={personalInformation.linkedIn.url} rel="noreferrer" color="inherit" variant="body2">
-          <FontAwesomeIcon icon={faLinkedin} />
-          <span>{personalInformation.linkedIn.display}</span>
-        </a>
-      </label>
-      <label>
-        <a className="contact-info-link" target="_blank" href={personalInformation.github.url} rel="noreferrer" color="inherit" variant="body2">
-          <FontAwesomeIcon icon={faGithub} />
-          <span>{personalInformation.github.display}</span>
         </a>
       </label>
     </>
