4a5aec14bfdcbf759563d9618cd049a353a71f1b
Mark Tutkowski <tutkows1@gmail.com>
2023-03-13 12:27:58 -0500

    Update education section details

--- src/data/education.js
+++ src/data/education.js
@@ -4,12 +4,20 @@ const eduction = [
     institution: 'University of Illinois Urbana-Champaign',
     startDate: 'Aug 2021',
     endDate: 'Present',
+    details: [
+      "GPA: 4.00",
+    ],
   },
   {
     degree: 'Computer Engineering, BS',
     institution: 'Milwaukee School of Engineering',
     startDate: 'Sept 2011',
     endDate: 'May 2015',
+    details: [
+      "GPA: 3.29, Major Specific GPA: 3.52",
+      "Graduated with Honors",
+      "Minor in Mathematics",
+    ],
   },
 ];
 
