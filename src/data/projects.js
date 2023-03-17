7cd696db89d06d7c6dec91b59f86f57afc1200ad
Mark Tutkowski <tutkows1@gmail.com>
2023-03-16 20:50:33 -0500

    refresh resume

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    name: 'Portfolio',
+    url: 'https://www.tutkowski.com',
+    details: [
+      "A portfolio which showcases some of my personal projects",
+      "Created with: React.js",
+    ],
+  },
+  {
+    name: 'Weather',
+    url: 'https://www.weather.tutkowski.com',
+    details: [
+      "A weather application that displays the current weather and forecast",
+      "Created with: Angular, Azure Functions, and GraphQL",
+    ],
+  },
+  {
+    name: 'News Word Cloud',
+    url: 'https://www.news.tutkowski.com',
+    details: [
+      "An application which generates a word cloud from recent news articles",
+      "Created with: Angular, D3.js, and Azure Functions",
+    ],
+  },
+  {
+    name: 'Resume',
+    url: 'https://www.resume.tutkowski.com',
+    details: [
+      "My resume written with web technologies",
+      "Created with: React.js",
+    ],
+  },
+];
+
+export default projects;
